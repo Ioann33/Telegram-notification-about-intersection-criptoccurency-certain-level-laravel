ad9f05fca0eb9b05c498c1a35e0664cad0db9bc0
Ioann <ivan.karmalyk2017@gmail.com>
2022-07-02 00:47:59 +0300

    added model for further Object

---
+++ app/Models/InnerObjModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+
+
+class InnerObjModel
+{
+   public $id;
+   public $name;
+   public $prise;
+   public $dir;
+   public $message;
+}
---
+++ app/Models/Note.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasFactory;
+    protected $table = 'notes';
+
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'prise',
+        'dir',
+        'message',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
